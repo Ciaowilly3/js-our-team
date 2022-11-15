a3a90f0f2068a7b5c16826c3a695b6b528e73517
Ciaowilly3 <muscomassi@gmail.com>
2022-11-15 15:11:03 +0100

    milestone complete

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const membersArray= [
+    {
+        fullName : "Wayne Barnett",
+        Role : "Founder & CEO",
+        picture : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        fullName : "Angela Caroll",
+        Role : "Chief Editor",
+        picture : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        fullName : "Walter Gordon",
+        Role : "Office Manager",
+        picture : "walter-gordon-office-manager.jpg"
+    },
+    {
+        fullName : "Angela Lopez",
+        Role : "Social Media Manager",
+        picture : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        fullName : "Scott Estrada",
+        Role : "Developer",
+        picture : "scott-estrada-developer.jpg"
+    },
+    {
+        fullName : "Barbara Ramos",
+        Role : "Graphic Designer",
+        picture : "barbara-ramos-graphic-designer.jpg"
+    },
+]
+// così stampo l'intero array compresi gli oggetti all'interno
+console.log(membersArray);
+
+// così stampo ogni singolo elemento dell'array, quindi un oggetto alla volta
+for (let i=0; i<membersArray.length; i++){
+    console.log(membersArray[i]);
+}
+
+// stampo nome, ruolo, e immagine in 3 colonne diverse nell'html per renderlo più ordinato possibile
+// per farlo creo 3 nodi di collegamento diversi per ogni colonna
+
+const nameContainerEl = document.querySelector(".name-container");
+const roleContainerEl = document.querySelector(".role-container");
+const imgContainerEl = document.querySelector(".img-container");
+
+
+
+for (let i=0; i<membersArray.length; i++){
+    nameContainerEl.innerHTML += `<div class="text-center">${membersArray[i].fullName}</div>`;
+}
+
+for (let i=0; i<membersArray.length; i++){
+    roleContainerEl.innerHTML += `<div class="text-center">${membersArray[i].Role}</div>`;
+}
+
+for (let i=0; i<membersArray.length; i++){
+    imgContainerEl.innerHTML += `<div class="text-center">${membersArray[i].picture}</div>`;
+}
